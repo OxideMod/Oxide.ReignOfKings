4b19df3c6e55b0029610b22e03abe5afed834c0b
Luke Spragg <luke@spragg.me>
2017-11-18 23:06:43 -0500

    Initial commit of split/migrated base

---
+++ Oxide.ReignOfKings/ReignOfKingsPluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using Oxide.Core.Plugins;
+using System;
+
+namespace Oxide.Game.ReignOfKings
+{
+    /// <summary>
+    /// Responsible for loading Reign of Kings core plugins
+    /// </summary>
+    public class ReignOfKingsPluginLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[] { typeof(ReignOfKingsCore) };
+    }
+}
